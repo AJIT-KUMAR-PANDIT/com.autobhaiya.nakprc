d75aa61bf89542671712487b183fce273dccdb9f
AJIT-KUMAR-PANDIT <ajitkumarpandit1999@gmail.com>
2026-01-18 14:43:44 +0530

    mnzm

--- src/components/Shared/Header.jsx
+++ src/components/Shared/Header.jsx
@@ -6,23 +6,25 @@ export default function Header() {
     <header className="sticky top-0 z-20 bg-background-light/95 dark:bg-background-dark/95 backdrop-blur-sm px-4 pt-2 pb-0.5 border-b border-gray-100 dark:border-gray-800">
       <div className="flex items-center justify-between">
         {/* Brand */}
-        <div className="flex items-center gap-2">
-          <div className="h-21 w-21">
-            <img
-              src="/logo.png"
-              alt="Auto Bhaiya Logo"
-              className="h-full w-full object-cover"
-            />
+        <a href="/">
+          <div className="flex items-center gap-2">
+            <div className="h-21 w-21">
+              <img
+                src="/logo.png"
+                alt="Auto Bhaiya Logo"
+                className="h-full w-full object-cover"
+              />
+            </div>
+            <div className="flex flex-col">
+              <h1 className="text-xl font-bold tracking-tight text-text-main dark:text-white">
+                Auto Bhaiya
+              </h1>
+              <span className="text-xs font-medium text-gray-500 dark:text-gray-400">
+                Partner App
+              </span>
+            </div>
           </div>
-          <div className="flex flex-col">
-            <h1 className="text-xl font-bold tracking-tight text-text-main dark:text-white">
-              Auto Bhaiya
-            </h1>
-            <span className="text-xs font-medium text-gray-500 dark:text-gray-400">
-              Partner App
-            </span>
-          </div>
-        </div>
+        </a>
         {/* Driver Profile & Badge */}
         <div className="flex items-center gap-3 bg-white dark:bg-card-dark rounded-full pl-3 pr-1 py-1 shadow-sm border border-gray-100 dark:border-gray-700">
           <div className="flex flex-col items-end mr-1">
